772ee3dbe9feffd22da9d5542b35340d278121d8
Dex <dexter@metalytics.dev>
2024-08-09 15:25:43 -0500

    Fix star imports / update langchain examples (#10)
    * fix star imports
    
    * refactor langchain examples
    
    * Apply suggestions from code review

--- examples/langchain/04-human_as_tool_onboarding.py
+++ examples/langchain/04-human_as_tool_onboarding.py
@@ -3,13 +3,14 @@ from dotenv import load_dotenv
 from langchain.agents import AgentType, initialize_agent
 from langchain_openai import ChatOpenAI
 
+from examples.langchain.channels import (
+    dm_with_summer_intern,
+    dm_with_head_of_marketing,
+    dm_with_ceo,
+)
 from functionlayer.core.approval import (
     ApprovalMethod,
     FunctionLayer,
-)
-from functionlayer.core.types import (
-    ContactChannel,
-    SlackContactChannel,
 )
 
 load_dotenv()
@@ -20,7 +21,8 @@ task_prompt = """
 
 You are the email onboarding assistant. You check on the progress customers
 are making and get other information, then based on that info, you
-send friendly and encouraging emails to customers to help them
+send friendly and encouraging emails to customers to help them fully onboard
+into the product.
 
 Before sending an email, you check with the head of marketing for feedback,
 and incorporate that feedback into your email before sending. You repeat the
@@ -35,11 +37,12 @@ def get_info_about_customer(customer_email: str) -> str:
     """get info about a customer"""
     return """
     This customer has completed most of the onboarding steps,
-    but still needs to invite a few team members before they can be considered fully onboarded
+    but still needs to invite a few team members before they can be 
+    considered fully onboarded
     """
 
 
-@fl.require_approval()
+@fl.require_approval(contact_channel=dm_with_ceo)
 def send_email(to: str, subject: str, body: str) -> str:
     """Send an email to a user"""
     return f"Email sent to {to} with subject: {subject}"
@@ -50,22 +53,12 @@ tools = [
     langchain_tools.StructuredTool.from_function(send_email),
     langchain_tools.StructuredTool.from_function(
         fl.human_as_tool(
-            contact_channel=ContactChannel(
-                slack=SlackContactChannel(
-                    channel_or_user_id="C07BU3B7DBM",
-                    context_about_channel_or_user="a DM with the head of marketing",
-                ),
-            )
+            contact_channel=dm_with_head_of_marketing,
         )
     ),
     langchain_tools.StructuredTool.from_function(
         fl.human_as_tool(
-            contact_channel=ContactChannel(
-                slack=SlackContactChannel(
-                    channel_or_user_id="C07BU3B7DBM",
-                    context_about_channel_or_user="a DM with the summer intern",
-                ),
-            )
+            contact_channel=dm_with_summer_intern,
         )
     ),
 ]
