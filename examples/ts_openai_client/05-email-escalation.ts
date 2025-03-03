aac87ecd0fd85774018f845a85baebc622da2beb
dexhorthy <dexter@humanlayer.dev>
2025-03-03 12:15:10 -0800

    tweak wording/naming/etc

--- examples/ts_openai_client/05-email-escalation.ts
+++ examples/ts_openai_client/05-email-escalation.ts
@@ -1,6 +1,6 @@
 import { FunctionCall, humanlayer } from "humanlayer";
 
-const hl = humanlayer({ verbose: true, runId: "ts-imperative-fetch-based" });
+const hl = humanlayer({ verbose: true, runId: "email-escalation" });
 
 const demoEscalation = async () => {
   const call: FunctionCall = await hl.createFunctionCall({
@@ -16,13 +16,18 @@ const demoEscalation = async () => {
     },
   });
 
+  // EXAMPLE - escalate immediately after 5 seconds
+  // in reality, this would happen after some longer amount of time has passed
+  await new Promise((resolve) => setTimeout(resolve, 1000 * 5));
+
   const escalated_call: FunctionCall = await hl.escalateEmailFunctionCall(
     call.call_id,
     {
       escalation_msg: "please take a look because it's been too long",
       additional_recipients: [
         {
-          address: process.env.HL_EXAMPLE_SECOND_CONTACT_EMAIL || "",
+          address:
+            process.env.HL_EXAMPLE_SECOND_CONTACT_EMAIL || "dan@humanlayer.dev",
           field: "to",
         },
       ],
