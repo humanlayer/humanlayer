8ee5fb9e2f473d09bdd0d395b58c82944b73c6f7
Allison Durham <allison@humanlayer.dev>
2025-06-16 11:46:53 -0700

    add react

--- humanlayer-wui/src/components/ui/collapsible.tsx
+++ humanlayer-wui/src/components/ui/collapsible.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react'
 import * as CollapsiblePrimitive from '@radix-ui/react-collapsible'
 
 function Collapsible({ ...props }: React.ComponentProps<typeof CollapsiblePrimitive.Root>) {
